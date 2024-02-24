801eb3c7e72fd3d8d2cb4de3e9eb5123546d17c7
Rithik <rithik.chdry@gmail.com>
2024-02-23 22:23:32 -0500

    fixing filter transition bug

--- src/script.js
+++ src/script.js
@@ -72,6 +72,10 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
   for (let i = 0; i < filterItems.length; i++) {
+    filterItems[i].classList.remove("active");
+    void filterItems[i].offsetWidth;
+  }
+  for (let i = 0; i < filterItems.length; i++) {
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
