d88b5d94f8bd1ccc4de1e428308ee19268482b9f
Rithik <rithik.chdry@gmail.com>
2025-02-16 17:51:23 -0500

    revamp init

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    domains: [
+      'imagizer.imageshack.com',
+      'i.imgur.com',
+      'images.unsplash.com',
+      'media.giphy.com',
+      'cdn-images-1.medium.com',
+      'github.com',
+      'd112y698adiu2z.cloudfront.net'
+    ],
+  },
+}
