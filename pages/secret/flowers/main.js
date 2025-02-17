2641fff177df2760db80d59679a5f4fe6040d8c1
Rithik <rithik.chdry@gmail.com>
2025-02-17 03:46:54 -0500

    vercel error fix

--- pages/secret/flowers/main.js
+++ pages/secret/flowers/main.js
@@ -1,6 +1,12 @@
-onload = () => {
+import { useEffect } from "react";
+
+export default function Main() {
+  useEffect(() => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
-  };
+  }, []);
+
+  return null; // Since there's no UI, return nothing
+}
